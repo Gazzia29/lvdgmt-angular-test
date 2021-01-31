2e12b062b6d496c0ed08ba8c1969d6d72ea4de98
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2021-01-31 19:23:00 +0100

    quickstart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './pages/game/game.component';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent },
+  { path: 'game', component: GameComponent },
+  { path: '**', component: NotFoundComponent }, // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { GameComponent } from './pages/game/game.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainMenuComponent, NotFoundComponent, GameComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
