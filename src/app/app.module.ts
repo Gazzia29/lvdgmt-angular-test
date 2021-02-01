50ccb60e1f89fb9be1dcadbbe6c4c885a20b659a
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2021-02-01 12:20:03 +0100

    Refactorized imports with aliases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainMenuComponent } from './pages/main-menu/main-menu.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { GameComponent } from './pages/game/game.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { RatioContainerComponent } from './components/ratio-container/ratio-container.component';
-import { BackgroundComponent } from './components/background/background.component';
-import { TextContentComponent } from './components/text-content/text-content.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { MainMenuComponent } from '@pages/main-menu/main-menu.component';
+import { NotFoundComponent } from '@pages/not-found/not-found.component';
+import { GameComponent } from '@pages/game/game.component';
+import { NavBarComponent } from '@components/nav-bar/nav-bar.component';
+import { RatioContainerComponent } from '@components/ratio-container/ratio-container.component';
+import { BackgroundComponent } from '@components/background/background.component';
+import { TextContentComponent } from '@components/text-content/text-content.component';
 
 @NgModule({
   declarations: [
