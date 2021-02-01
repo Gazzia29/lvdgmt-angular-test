29727d7939804753d4fd91e2025e2ed7d0b184f3
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2021-02-01 12:06:49 +0100

    added components

---
+++ src/app/components/ratio-container/ratio-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ratio-container',
+  templateUrl: './ratio-container.component.html',
+  styleUrls: ['./ratio-container.component.scss']
+})
+export class RatioContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
